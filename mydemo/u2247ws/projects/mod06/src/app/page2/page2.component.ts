9357707fe3e5f51f23fcf10cc8c2b7722153f400
ZihLiangLin <Realman@DESKTOP-FPS2A21>
2021-12-28 16:32:22 +0800

    add Template Forms

---
+++ mydemo/u2247ws/projects/mod06/src/app/page2/page2.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { MeetingRoom } from '../meeting-room';
+
+@Component({
+  selector: 'app-page2',
+  templateUrl: './page2.component.html',
+  styles: [
+  ]
+})
+export class Page2Component implements OnInit {
+
+  public meetingRoom: MeetingRoom;
+  public capacity = ['5人', '10人', '20人', '30人', '40人'];
+
+  constructor() { 
+    this.meetingRoom = new MeetingRoom(
+      101, '哥多華', this.capacity[2], false, true
+    );
+  }
+
+  ngOnInit(): void {
+  }
+
+  // onSubmit() {
+  //   this.meetingRoom = {
+  //     id: this.meetingRoom.id,
+  //     name: this.meetingRoomForm.get('name')?.value,
+  //     size: this.meetingRoomForm.get('size')?.value,
+  //     projector: this.meetingRoomForm.get('projector')?.value,
+  //     TV: this.meetingRoomForm.get('TV')?.value
+  //   };
+  // }
+}
