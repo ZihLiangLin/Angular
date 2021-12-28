f9e18e3c95f837a9ea50eb9c4795e4fd89cd72ef
ZihLiangLin <Realman@DESKTOP-FPS2A21>
2021-12-28 15:26:39 +0800

    add self-definition pipe

---
+++ mydemo/u2247ws/projects/mod05/src/app/usd2-twd.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'uSD2TWD'
+})
+export class USD2TWDPipe implements PipeTransform {
+
+  transform(value: number, ...args: unknown[]): number {
+    return value * 27.9;
+  }
+
+}
