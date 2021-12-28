00ebfe3f85de595f91f4f662aa623472c734f3a3
ZihLiangLin <Realman@DESKTOP-FPS2A21>
2021-12-28 14:18:51 +0800

    add @ViewChild

--- mydemo/u2247ws/projects/mod05/src/app/app.component.ts
+++ mydemo/u2247ws/projects/mod05/src/app/app.component.ts
@@ -7,11 +7,19 @@ import { CheckStockComponent } from './check-stock/check-stock.component';
   styles: []
 })
 export class AppComponent {
-// @ViewChild("title1") h1: ElementRef | any;
-// @ViewChild(CheckStockComponent) checkStock: CheckStockComponent;
+  @ViewChild("title1") h1: ElementRef | any;
+  @ViewChild(CheckStockComponent) checkStock: CheckStockComponent | any ;
 
   title = '購物車';
   Qty: number = 12;
   Stock: number = 10;
 
+  ngAfterViewInit(){
+    console.log(this.h1.nativeElement);
+    console.log("ngAfterViewInit : ", this.checkStock);
+  }
+
+  ngAfterViewChecked(){
+    console.log("ngAfterViewChecked : ", this.checkStock);
+  }
 }
