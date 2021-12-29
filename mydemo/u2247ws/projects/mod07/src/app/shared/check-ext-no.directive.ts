9d700e2465244e57e2f24cee4b51111ecfecfdd9
ZihLiangLin <Realman@DESKTOP-FPS2A21>
2021-12-29 11:09:19 +0800

    add checkExtNo validators

---
+++ mydemo/u2247ws/projects/mod07/src/app/shared/check-ext-no.directive.ts
@@ -0,0 +1,24 @@
+import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator, Validators } from '@angular/forms';
+
+@Directive({
+  selector: '[CheckExtNo]',
+  providers:[{
+    provide:NG_VALIDATORS,
+    useExisting:CheckExtNoDirective,
+    multi:true
+  }]
+})
+
+export class CheckExtNoDirective implements Validator{
+
+  constructor() { }
+  validate(control: AbstractControl): ValidationErrors | null{
+    let extNo: number = parseInt(control.value);
+    let result = null;
+    if(extNo < 1000 || extNo > 1999)
+      result={"CheckExtNo":extNo};
+    return result;
+  }
+
+}
