f34317edc90fa9db959e5be937d88362110ad94c
ZihLiangLin <Realman@DESKTOP-FPS2A21>
2021-12-29 11:31:23 +0800

    add checkEndDate validator

---
+++ mydemo/u2247ws/projects/mod07/src/app/shared/check-end-date.directive.ts
@@ -0,0 +1,37 @@
+import { Directive, Input } from '@angular/core';
+import { AbstractControl, FormGroup, NG_VALIDATORS, ValidationErrors, Validator, Validators } from '@angular/forms';
+
+@Directive({
+  selector: '[CheckEndDate]',
+  providers:[{
+    provide:NG_VALIDATORS,
+    useExisting:CheckEndDateDirective,
+    multi:true
+  }]
+})
+export class CheckEndDateDirective implements Validator{
+  @Input('CheckEndDate') mustGT: string[] = [];
+  constructor() { }
+  validate(fg: FormGroup): ValidationErrors | null {
+    return MustGT(this.mustGT[0], this.mustGT[1])(fg);
+  }
+
+}
+
+export function MustGT(startCtlName:string, endCtlName: string) {
+  return (fg:FormGroup) => {
+    const startCtl = fg.controls[startCtlName];
+    const endCtl = fg.controls[endCtlName];
+    if(!startCtl || ! endCtl)
+      return null;
+    if(endCtl.errors && !endCtl.errors.MustGT)
+      return null;
+    let startDate = Date.parse(startCtl.value);
+    let endDate = Date.parse(endCtl.value);
+    if(endDate <= startDate)
+      endCtl.setErrors({MustGT:true});
+    else
+      endCtl.setErrors(null);
+    return null;
+  }
+}
